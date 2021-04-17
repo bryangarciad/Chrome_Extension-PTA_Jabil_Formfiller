acddef1640e64d6b779dfc646d8257f2c4517ede
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2021-04-16 18:59:18 -0600

    first phase working; data upload, htm table process, save state, mark sent data

--- dist/content.js
+++ dist/content.js
@@ -3,5 +3,9 @@ console.log("Chrome Extension ready to go!");
 chrome.runtime.onMessage.addListener(replace);
 //Replace
 function replace(message, sender, sendresponse) {
+    //use axxios request to update DOM
+    //use ptaData to fill the proper inputs
+    //return response
     console.log(message);
+    sendresponse('yeaaaaaaah boy')
 }
--- src/index.js
+++ src/index.js
@@ -8,20 +8,34 @@ const fileInput = document.getElementById("PTA_CSV")
 
 // Global required vars
 let PtaObjectArr = [];
+const doubleTagRegex = /<td>|<\/td>/g;
 
+/******************** */
 // row click listiner
+/******************** */
 const rowClicked = (i) => {
     //axios request
     //send message to background with the info; background worker will hanlde the target DOM
-    if (confirm(`You're about to send PTA: ${PtaObjectArr[i].pta_id} data`)) {
+    if (confirm(`You're about to send PTA: ${PtaObjectArr[i].pta_id}`)) {
+        let cleanObject = Object.assign({}, PtaObjectArr[i]);
+        Object.keys(cleanObject).forEach( k => {
+            cleanObject[k] = k !== 'sent' ? cleanObject[k].replaceAll(doubleTagRegex, '') : cleanObject[k];
+        })
+        
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {csvData: PtaObjectArr[i], axiosData: 'undefined'}, function(response) {});  
+            chrome.tabs.sendMessage(tabs[0].id, {csvData: cleanObject, axiosData: 'undefined'}, function(response) {alert(response)});  
         });
+        PtaObjectArr[i].sent = true;
+        //update local html table and storage with the new data
+        localStorage.setItem('ptaObjectData', JSON.stringify(PtaObjectArr))
+        ArrToHtmlTableBody(PtaObjectArr, false)
       } else {
       } 
 }
 
+/*********************************************** */
 //add listener for click event to each row in table
+/********************************************** */
 const addListenerToRows = () => {
     let rows = document.getElementsByClassName('ptaRow');
     for(let i = 0; i < rows.length; i++ ) {
@@ -29,25 +43,58 @@ const addListenerToRows = () => {
     }
 }
 
+/******************************************* */
+// Transform Array Object into HTML table Body
+/***************************************** */
+const ArrToHtmlTableBody = (data, fromLoad) => {
+    let tableBody = ''
+    let newData = []
+    if(fromLoad){
+        newData = data.map( item => {
+            Object.keys(item).forEach(k => {
+                if(k !== 'sent') {
+                    item[k] = `<td>${item[k]}</td>`
+                } else {
+                    item[k] = item[k]
+                }
+                
+            })
+            return item;
+        })
+    }
+    else{
+        newData = data
+    }
+    newData.forEach(item => {
+        if(item.sent) {
+            tableBody += `<tr class="red ptaRow"> ${item.pta_id.replace(/\s/g, '')}${item.peso_bruto}${item.imp_guia}${item.no_guia}${item.no_factura}${item.pedimento}${item.date}${item.impo_expo}</tr>`
+        } else {
+            tableBody += `<tr class="white ptaRow"> ${item.pta_id.replace(/\s/g, '')}${item.peso_bruto}${item.imp_guia}${item.no_guia}${item.no_factura}${item.pedimento}${item.date}${item.impo_expo}</tr>`
+        }
+    })
+    
+    document.getElementById('tbody').innerHTML = tableBody
+    addListenerToRows();
+}
+
+/********************************* */
 //check if data is already stores
-if (localStorage.getItem('ptaTableData') && localStorage.getItem('ptaObjectData')) {
+/******************************** */
+if (localStorage.getItem('ptaObjectData')) {
     try {
         PtaObjectArr = JSON.parse(localStorage.getItem('ptaObjectData'))
-
+        ArrToHtmlTableBody(PtaObjectArr, false);
     } catch(e) {
         alert(e); // error in the above strings
         localStorage.removeItem('ptaObjectData')
     }
-    document.getElementById('tbody').innerHTML = localStorage.getItem('ptaTableData')
-    addListenerToRows();
+    // document.getElementById('tbody').innerHTML = localStorage.getItem('ptaTableData')
+    // addListenerToRows();
 }
 
-// Transform Array Object into HTML table Body
-const ArrToHtmlTableBody = (data) => {
-    
-}
-
+/*********************** */
 //File reader with FR
+/********************** */
 const readFile = function () {
     var reader = new FileReader();
     reader.onload = function () {
@@ -65,38 +112,41 @@ const readFile = function () {
         // prepare each item individually for HTML adding TD tag
         ptaArr = ptaArr.map( (item, count) => {
             let cleanItem = item.indexOf('CI') !== -1 ? item.slice(item.indexOf('CI'), item.length) : item.replaceAll(' ', '');
-            return `<td>${cleanItem}</td>`
+            // return `<td>${cleanItem}</td>`
+            return cleanItem;
         })
 
         // Splice Array into groups of 8 (row of 8 items)
         while(ptaArr.length) tableBodyArr.push(ptaArr.splice(0,8));
 
         // Joining every item into a single string
-        let TBody = tableBodyArr.map( (item, count) => {
-            const doubleTagRegex = /<td>|<\/td>/g;
-
+        tableBodyArr.map( (item, count) => {
+            // const doubleTagRegex = /<td>|<\/td>/g;
             PtaObjectArr.push({
-                'pta_id': item[0] ? item[0].replaceAll(doubleTagRegex, '') : '',  
-                'peso_bruto': item[1] ? item[1].replaceAll(doubleTagRegex, '') : '',
-                'imp_guia': item[2] ? item[2].replaceAll(doubleTagRegex, '') : '',
-                'no_guia': item[3] ? item[3].replaceAll(doubleTagRegex, '') : '',
-                'no_factura': item[4] ? item[4].replaceAll(doubleTagRegex, '') : '',
-                'pedimento': item[5] ? item[5].replaceAll(doubleTagRegex, '') : '',
-                'date': item[6] ? item[6].replaceAll(doubleTagRegex, '') : '',
-                'impo_expo': item[7] ? item[7].replaceAll(doubleTagRegex, '') : ''
+                'pta_id': item[0], 
+                'peso_bruto': item[1],
+                'imp_guia': item[2],
+                'no_guia': item[3],
+                'no_factura': item[4],
+                'pedimento': item[5],
+                'date': item[6],
+                'impo_expo': item[7],
+                'sent': false
             })
 
-            return `<tr class="ptaRow" id="${count}"> ${item.join('')} </tr>`
+            // return `<tr class="ptaRow" id="${count}"> ${item.join('')} </tr>`
         })
 
         //Save Local Storage table data and object data to keep the app state
-        localStorage.setItem('ptaTableData', TBody.reduce((acc, currentValue) => acc + currentValue))
+        // localStorage.setItem('ptaTableData', TBody.reduce((acc, currentValue) => acc + currentValue))
         localStorage.setItem('ptaObjectData', JSON.stringify(PtaObjectArr))
         // Array to String with reducer acction
-        document.getElementById('tbody').innerHTML = TBody.reduce((acc, currentValue) => acc + currentValue)
+        // document.getElementById('tbody').innerHTML = TBody.reduce((acc, currentValue) => acc + currentValue)
     };
     reader.onloadend = () => {
-        addListenerToRows();
+        ArrToHtmlTableBody(PtaObjectArr, true);
+
+        // addListenerToRows();
     }
     // start reading the file. When it is done, calls the onload event defined above.
     reader.readAsBinaryString(fileInput.files[0]);
