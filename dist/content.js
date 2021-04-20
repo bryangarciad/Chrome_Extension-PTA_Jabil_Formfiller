dd212e12c6dcfa6d7952754b7543c2935e1d5e5f
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2021-04-19 21:29:57 -0600

    axios working 8

--- dist/content.js
+++ dist/content.js
@@ -11,23 +11,25 @@ function replace(message, sender, sendresponse) {
     target.innerHTML = ''
     target.innerHTML = message.axiosData.data
     //fill the rest of the data with the message csv
-    const weight = document.getElementById('txtRealWeight')
-    const no_guia = document.getElementById('txtRealAirWayBill')
-    const invoice = document.getElementById('txtInvoice')
-    const date = document.getElementById('txtCustomsEntryDate')
-    const Freight = document.getElementById('txtRealFreight')
-
-    // weight.value = message.csvData.peso_bruto;
-    // no_guia.value = message.csvData.no_guia;
-    // invoice.value = message.csvData.no_factura
-    // Freight.value = message.csvData.imp_guia
-    // date.value = message.csvData.date
-    weight.value = 'ok Weight'
-    no_guia.value =  'ok guia'
-    invoice.value =  'ok invoice'
-    Freight.value =  'ok imp guia'
-    date.value = 'ok  date'
+    setTimeout(() => {
+        const weight = document.getElementById('txtRealWeight')
+        const no_guia = document.getElementById('txtRealAirWayBill')
+        const invoice = document.getElementById('txtInvoice')
+        const date = document.getElementById('txtCustomsEntryDate')
+        const Freight = document.getElementById('txtRealFreight')
     
+        // weight.value = message.csvData.peso_bruto;
+        // no_guia.value = message.csvData.no_guia;
+        // invoice.value = message.csvData.no_factura
+        // Freight.value = message.csvData.imp_guia
+        // date.value = message.csvData.date
+        weight.value = 'ok Weight'
+        no_guia.value =  'ok guia'
+        invoice.value =  'ok invoice'
+        Freight.value =  'ok imp guia'
+        date.value = 'ok  date'
+    }, 1000)
+   
     console.log(message);
     sendresponse('yeaaaaaaah boy')
 }
