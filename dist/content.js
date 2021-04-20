caa6b5790e6fcfb7cd356e4a6c90471c5cd327f9
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2021-04-19 20:24:50 -0600

    axios working 2

--- dist/content.js
+++ dist/content.js
@@ -7,11 +7,11 @@ function replace(message, sender, sendresponse) {
     //use axxios request to update DOM
     //use ptaData to fill the proper inputs
     //return response
-    const target = document.getElementsByName('_middleframe2')
+    const target = document.querySelector('[name="_middleframe2"]')
     target.innerHTML = ''
     target.innerHTML = message.axiosData.data
     //fill the rest of the data with the message csv
-    
+
     console.log(message);
     sendresponse('yeaaaaaaah boy')
 }
